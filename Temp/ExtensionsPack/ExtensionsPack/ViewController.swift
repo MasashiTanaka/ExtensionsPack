bc04952ac54d6361b6a10dc4331e4d8fee0f40b1
Masashi Tanaka <tanaka@seesaa.co.jp>
2019-02-06 20:39:56 +0900

    add template project

---
+++ Temp/ExtensionsPack/ExtensionsPack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtensionsPack
+//
+//  Created by Masashi Tanaka on 2019/02/06.
+//  Copyright © 2019年 Masashi Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
