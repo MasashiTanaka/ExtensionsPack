0c309fc0749e25d2c05d6120c4f96f3d18fbc90c
Masashi Tanaka <tanaka@seesaa.co.jp>
2019-02-07 13:34:30 +0900

    fix relative file

--- ExtensionsPack/Extensions/UIViewController+Extension.swift
+++ ExtensionsPack/Extensions/UIViewController+Extension.swift
@@ -1,8 +1,8 @@
 //
-//  StoryBoardHelper.swift
+//  UIViewController+Extension.swift
 //  ExtensionsPack
 //
-//  Created by Masashi Tanaka on 2019/02/06.
+//  Created by Masashi Tanaka on 2019/02/07.
 //  Copyright © 2019年 Masashi Tanaka. All rights reserved.
 //
 
@@ -11,6 +11,7 @@ import UIKit
 public protocol StoryBoardHelper {}
 
 extension StoryBoardHelper where Self: UIViewController {
+
     public static func instantiate() -> Self {
         let storyboard = UIStoryboard(name: self.className, bundle: nil)
         return storyboard.instantiateViewController(withIdentifier: self.className) as! Self
@@ -20,6 +21,7 @@ extension StoryBoardHelper where Self: UIViewController {
         let storyboard = UIStoryboard(name: storyboard, bundle: nil)
         return storyboard.instantiateViewController(withIdentifier: self.className) as! Self
     }
+
 }
 
 extension UIViewController: StoryBoardHelper {}
